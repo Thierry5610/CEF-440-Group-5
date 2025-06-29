fdc0452aac29ac44c8fef46fa268964a67a02f4a
McTech6 <tiddingramsey@gmail.com>
2025-06-29 21:40:48 +0100

    Changes done

--- Code/WayWatch Backend/src/server.js
+++ Code/WayWatch Backend/src/server.js
@@ -9,6 +9,6 @@ initSocket(server);
 startTrafficMonitor(); // Start background job
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => { // <-- Change applied here: added '0.0.0.0'
   console.log(`Server running on port ${PORT}`);
 });
